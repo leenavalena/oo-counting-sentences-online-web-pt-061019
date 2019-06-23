2be6480808c0b3b940b3eb79dda5a240c70c1631
Valena Del Valle <valenadelvalle4@gmail.com>
2019-06-23 23:15:52 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -16,6 +16,7 @@ class String
 
   def count_sentences
     sentence_array = self.split (/[.?!]/)
+    sentence_array.delete_if{|sentence| sentence.empty?}
     return sentence_array.count
   end
 end
